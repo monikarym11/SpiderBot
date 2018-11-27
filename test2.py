455a05fa0e4260a0231f27b96540f77830d67dd9
Monika Rymsza <monika.rymsz@tieto.com>
2018-11-27 14:04:53 +0100

    move_forward should work

--- test2.py
+++ test2.py
@@ -8,8 +8,8 @@ class SpiderMoves():
         self.addr = 0x40
         #self.legs = Legs()
         self.x = Pair()
-        #self.y = Pair()
-        #self.z = Pair()
+        self.y = Pair()
+        self.z = Pair()
         # with SMBusWrapper(1) as bus:
         #     bus.write_byte_data(self.addr, 0, 0x20) # enable the chip
         #     time.sleep(.25)
@@ -32,10 +32,15 @@ class SpiderMoves():
             
     def move_forward(self):
         #while(True):
-        for i in range(1,50):
+        self.x.modes= [1, -1, -1, 1, 1, 1]
+        self.y.modes = [-1, -1, -1, 1, 1, -1]
+        self.z.modes = [1, -1, -1, 1, 1, 1]
+        for i in range(1,200):
             self.x.move(1)
-            #self.y.move(-1)
-            #self.x.move(1)
+            self.y.move(-1)
+            self.z.move(1)
+            print('x: {} {} {}, y: {} {} {}, z: {} {} {}'.format(self.x.angle, self.x.lleg.angle, self.x.rleg.angle,self.y.angle,self.y.lleg.angle, self.y.rleg.angle,self.z.angle,self.z.lleg.angle, self.z.rleg.angle))
+            
         #self.calibrate()
    # def move_round(self):
 
@@ -52,15 +57,14 @@ class Pair():
     def __init__(self):
         self.angle = 0
         #self.angles = list(range(-45,45))+list(range(45,-45,-1))
-        self.mode = [1, -1, -1, 1, 1, 1]
+        self.mode = 1
+        self.modes = []
         self.last = 0
-        self.prev = 0
-        self.lock = 1
+        self.prev = 0        
         self.addr = 0
         self.lleg = Leg('left')
         self.rleg = Leg('right')
-        self.lver = 1
-        self.rver = -1   
+        
     def calibrate(self):
         while(self.angle != 0):
             self.lleg.move(1)
@@ -70,35 +74,28 @@ class Pair():
         # if(self.angle != 0):
         #     self.lleg.move(self.last, mode)
         #     self.rleg.move(self.last, -mode)
+        if(self.lleg.lock):
+            self.lleg.move(self.last, mode)
+        if(self.rleg.lock):
+            self.rleg.move(self.last, mode)
+        self.angle = self.angle + self.modes[self.last]
         
-        self.lleg.move(self.last, mode, self.lock, self.lver)
-        self.rleg.move(self.last, mode, self.lock, -self.lver)
-        self.angle = self.angle + mode * self.mode[self.last]
-        if(self.angle >= 44 or self.angle <=-44):
-            self.lock = 0
-        else: 
-            self.lock = 1
         #move_angle(self.angle, self.addr)   
-        if(self.angle == 45):
-            self.last = 1
-            self.lver = -self.lver
-            #self.rver = -self.rver
-        elif(self.angle == -45):
+        if(self.angle >= 44):
+            self.last = 1            
+        elif(self.angle <= -44):
             self.last = 3
-            self.lver = -self.lver
-            #self.rver = -self.rver
         elif(self.angle == 0 and self.last == 1):
-            self.last = 2
-            #self.lver = -self.lver
-            #self.rver = -self.rver
+            self.last = 2            
         elif(self.angle == 0 and self.last == 3):
-            self.last = 4            
-            self.lver = -self.lver
-            #self.rver = -self.rver
-        elif(self.angle == 22 and self.last == 0):
+            self.last = 4
+        elif(abs(self.angle) == 22 and self.last == 0):
             self.last = 5
-            #self.lver = -self.lver
-            #self.rver = -self.rver
+        if(self.last == 2 or self.last == 4 or self.last == 5):
+            if(self.lleg.angle + self.rleg.angle == 0):
+                self.rleg.lock = True
+                self.lleg.lock = True
+        
         
         
 
@@ -109,29 +106,32 @@ class Leg():
             self.mode = 1
         elif(side =='right'):
             self.mode = -1
-
+        self.lock = True
         self.modes = [1,-self.mode, self.mode, self.mode, -self.mode, 1]
         #self.max = 
         self.min = -1
         self.addr = 0
-    def move(self, last, mode, lock, ver):
-        if(lock == 0 and ver ==-1):
-            print('last: {}, side: {}, angle: {}'.format(last, self.mode, self.angle))
-        else:
-            if(last != 0 and last != 5):
-                self.angle = self.angle + self.modes[last]
-            elif(last == 0):
-                self.angle = self.angle + (mode * self.mode)
-            elif(last == 5):
-                self.angle = self.angle - (mode * self.mode)
-                #move_angle(self.angle, self.addr)
-            #print last + self.angle
-            print('last: {}, side: {}, angle: {}'.format(last, self.mode, self.angle))
+    def move(self, last, mode):
+        
+        if(last != 0 and last != 5):
+            self.angle = self.angle + self.modes[last]*mode
+        elif(last == 0):
+            self.angle = self.angle + (mode * self.mode)
+        elif(last == 5):
+            self.angle = self.angle - (mode * self.mode)
+            #move_angle(self.angle, self.addr)
+        #print last + self.angle
+        #print('last: {}, side: {}, angle: {}'.format(last, self.mode, self.angle))
+        if(self.angle == -5):
+            self.lock = False
 
     ## Running this program will move the servo to 0, 45, and 90 degrees with 5 second pauses in between with a 50 Hz PWM signal.
 if __name__ == "__main__":
+    start = time.time()
     SMoves = SpiderMoves()
     SMoves.move_forward()
+    end = time.time()
+    print(end-start)
     #servo movement testing:
     # for i in range(1,5):
     #     SMoves.move_forward()
