02064fb5013f1c7fa4a5f8c19991a5696b403be1
Monika Rymsza <monika.rymsz@tieto.com>
2018-12-04 14:25:13 +0100

    turn right & left

--- test2.py
+++ test2.py
@@ -3,13 +3,14 @@
 import time
 
 class SpiderMoves():
-    def __init__(self):
-        #self.angles = list(range(1,90))+list(range(90,1,-1))
+    def __init__(self):    
+
         self.addr = 0x40
-        #self.legs = Legs()
+        
         self.x = Pair([0x06, 0x0A, 0x0E], [0x08, 0x0C, 0x10])
-        self.y = Pair([0x12, 0x16m 0x1A], [0x14, 0x18, 0x1C])
+        self.y = Pair([0x12, 0x16, 0x1A], [0x14, 0x18, 0x1C])
         self.z = Pair([0x1E, 0x22, 0x26], [0x20, 0x24, 0x28])
+        self.calibrate()
         # with SMBusWrapper(1) as bus:
         #     bus.write_byte_data(self.addr, 0, 0x20) # enable the chip
         #     time.sleep(.25)
@@ -18,33 +19,72 @@ class SpiderMoves():
         #     bus.write_byte_data(self.addr, 0xfe, 0x79) #changes the Prescale register value for 50 Hz, using the equation in the datasheet.
         #     bus.write_byte_data(self.addr, 0, 0x20) # enables the chip
 
-        #     time.sleep(.25)
-        #     bus.write_word_data(self.addr, 0x06, 0) # chl 0 start time = 0us
+    #     time.sleep(.25)
+    #     bus.write_word_data(self.addr, 0x06, 0) # chl 0 start time = 0us
                        
-        #     time.sleep(.25)
-        #     bus.write_word_data(self.addr, 0x08, 209) # chl 0 end time = 1.0ms (0 degrees)        
-    
-    #bus = smbus2.SMBus
+    #     time.sleep(.25)
+    #     bus.write_word_data(self.addr, 0x08, 209) # chl 0 end time = 1.0ms (0 degrees)        
+        
     def calibrate(self):
         self.x.calibrate()
         self.y.calibrate()
-        self.z.calibrate()
+        self.z.calibrate()    
         print('x: {} {} {}, y: {} {} {}, z: {} {} {}'.format(self.x.angle, self.x.lleg.angle, self.x.rleg.angle,self.y.angle,self.y.lleg.angle, self.y.rleg.angle,self.z.angle,self.z.lleg.angle, self.z.rleg.angle))
             
-    def move_forward(self):
-        #while(True):
-        self.x.modes= [1, -1, -1, 1, 1, 1, 0]
+    def move_forward(self):    
+        self.x.modes = [1, -1, -1, 1, 1, 1, 0]
         self.y.modes = [-1, -1, -1, 1, 1, -1, 0]
         self.z.modes = [1, -1, -1, 1, 1, 1, 0]
         for i in range(1,200):
             self.x.move(1)
             self.y.move(-1)
             self.z.move(1)
-        print('x: {} {} {}, y: {} {} {}, z: {} {} {}'.format(self.x.angle, self.x.lleg.angle, self.x.rleg.angle,self.y.angle,self.y.lleg.angle, self.y.rleg.angle,self.z.angle,self.z.lleg.angle, self.z.rleg.angle))
-        
-            
+        #print('x: {} {} {}, y: {} {} {}, z: {} {} {}'.format(self.x.angle, self.x.lleg.angle, self.x.rleg.angle,self.y.angle,self.y.lleg.angle, self.y.rleg.angle,self.z.angle,self.z.lleg.angle, self.z.rleg.angle))
+    def move_backwards(self):
+        self.x.modes = [-1, -1, -1, 1, 1, -1, 0]
+        self.y.modes = [1, -1, -1, 1, 1, 1, 0]
+        self.z.modes = [-1, -1, -1, 1, 1, -1, 0]
+        for i in range(1,200):
+            self.x.move(1)
+            self.y.move(-1)
+            self.z.move(1)
+        #print('x: {} {} {}, y: {} {} {}, z: {} {} {}'.format(self.x.angle, self.x.lleg.angle, self.x.rleg.angle,self.y.angle,self.y.lleg.angle, self.y.rleg.angle,self.z.angle,self.z.lleg.angle, self.z.rleg.angle))        
         #self.calibrate()
    # def move_round(self):
+    def turn_left(self):
+        
+        self.x.modes = [-1, -1, -1, 1, 1, -1, 0]
+        self.y.modes = [-1, -1, -1, 1, 1, -1, 0]
+        self.z.modes = [-1, -1, -1, 1, 1, -1, 0]
+        while(self.x.angle != self.x.min):
+            self.x.move_up(-1)            
+        while(self.y.angle != self.y.min):
+            self.y.move_up(-1)            
+        while(self.z.angle != self.z.min):
+            self.z.move_up(-1)
+        self.move_down()
+
+
+    def turn_right(self):
+        self.x.modes = [1, -1, -1, 1, 1, 1, 0]
+        self.y.modes = [1, -1, -1, 1, 1, 1, 0]
+        self.z.modes = [1, -1, -1, 1, 1, 1, 0]
+        while(self.x.angle != self.x.max):
+            self.x.move_up(1)
+        while(self.y.angle != self.y.max):
+            self.y.move_up(1)
+        while(self.z.angle != self.z.max):
+            self.z.move_up(1)
+        self.move_down()
+        
+    def move_down(self):
+        self.x.lleg.angle = self.x.lleg.min
+        self.x.rleg.angle = self.x.rleg.min
+        self.y.lleg.angle = self.y.lleg.min
+        self.y.rleg.angle = self.y.rleg.min
+        self.z.lleg.angle = self.z.lleg.min
+        self.z.rleg.angle = self.z.rleg.min
+        self.calibrate()
 
 # def move(self):        
 #         #for angle in self.angles:
@@ -59,13 +99,12 @@ class Pair():
     def __init__(self, start_addr, stop_addr):
         self.angle = 0
         self.start_addr = start_addr[0]
-        self.stop_addr = stop_addr[0]
-        #self.angles = list(range(-45,45))+list(range(45,-45,-1))
+        self.stop_addr = stop_addr[0]        
+        self.min = -44
+        self.max = 44
         self.mode = 1
         self.modes = []
         self.last = 0
-        self.prev = 0        
-        self.addr = 0
         self.lleg = Leg('left', start_addr[1], stop_addr[1])
         self.rleg = Leg('right', start_addr[2], stop_addr[2])
         
@@ -93,6 +132,7 @@ class Pair():
                 self.rleg.calibrate()
             #move_angle(self.addr)
             #print('p: {}, l: {}, r: {}'.format(self.angle, self.lleg.angle, self.rleg.angle))
+            self.last = 0
     
     def move(self, mode):  
         
@@ -103,9 +143,9 @@ class Pair():
         self.angle = self.angle + self.modes[self.last]
         
         #move_angle(self.angle, self.addr)   
-        if(self.angle >= 44):
+        if(self.angle >= self.max):
             self.last = 1            
-        elif(self.angle <= -44):
+        elif(self.angle <= self.min):
             self.last = 3
         elif(self.angle == 0 and self.last == 1):
             self.last = 2            
@@ -116,10 +156,28 @@ class Pair():
         if(self.last == 2 or self.last == 4 or self.last == 5):
             if(self.lleg.angle + self.rleg.angle == 0):
                 self.rleg.lock = True
-                self.lleg.lock = True
+                self.lleg.lock = True        
         
+    def move_up(self, mode):
         
+        self.lleg.move(self.last, self.lleg.mode)
         
+        self.rleg.move(self.last, self.rleg.mode)
+        self.angle = self.angle + self.modes[self.last]
+        
+        #move_angle(self.angle, self.addr)   
+        # if(self.angle >= self.max):
+        #     self.last = 1            
+        # elif(self.angle <= self.min):
+        #     self.last = 3
+        # elif(self.angle == 0 and self.last == 1):
+        #     self.last = 2            
+        # elif(self.angle == 0 and self.last == 3):
+        #     self.last = 4
+        if(abs(self.angle) == 22 and self.last == 0):
+            self.last = 5
+        #if(self.last == 2 or self.last == 4 or self.last == 5):
+              
 
 class Leg():
     def __init__(self, side, start_addr, stop_addr):
@@ -144,10 +202,10 @@ class Leg():
         elif(last == 5):
             self.angle = self.angle - (mode * self.mode)
         
-            #move_angle(self.angle, self.addr)
+        #move_angle(self.angle, self.addr)
         #print last + self.angle
         #print('last: {}, side: {}, angle: {}'.format(last, self.mode, self.angle))
-        if(self.angle == -5):
+        if(self.angle == self.min):
             self.lock = False
     def calibrate(self):
         self.angle = self.angle + (0-self.angle)/abs(self.angle)
@@ -155,8 +213,12 @@ class Leg():
 if __name__ == "__main__":
     start = time.time()
     SMoves = SpiderMoves()
-    SMoves.move_forward()
-    SMoves.calibrate()
+    #SMoves.move_forward()
+    #SMoves.calibrate()
+    #SMoves.move_backwards()
+    #SMoves.calibrate()
+    for i in range(1,5):
+        SMoves.turn_right()
     end = time.time()
     print(end-start)
     #servo movement testing:
